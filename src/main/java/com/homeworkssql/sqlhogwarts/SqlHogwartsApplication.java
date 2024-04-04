2f798dfb7d73c3849efcf82a7b4c757683871087
WalterMitty23 <23alexold23@gmail.com>
2024-04-04 06:26:51 -0400

    1.0

---
+++ src/main/java/com/homeworkssql/sqlhogwarts/SqlHogwartsApplication.java
@@ -0,0 +1,13 @@
+package com.homeworkssql.sqlhogwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlHogwartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlHogwartsApplication.class, args);
+	}
+
+}
