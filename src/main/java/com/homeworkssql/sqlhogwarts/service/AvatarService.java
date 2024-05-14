7af2d02f619295bb4b94b29b89d299aedda8c9eb
WalterMitty23 <23alexold23@gmail.com>
2024-05-14 13:21:22 -0400

    4.4 done

--- src/main/java/com/homeworkssql/sqlhogwarts/service/AvatarService.java
+++ src/main/java/com/homeworkssql/sqlhogwarts/service/AvatarService.java
@@ -3,6 +3,8 @@ package com.homeworkssql.sqlhogwarts.service;
 import com.homeworkssql.sqlhogwarts.model.Avatar;
 import com.homeworkssql.sqlhogwarts.repository.AvatarRepository;
 import com.homeworkssql.sqlhogwarts.repository.StudentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +19,8 @@ import java.nio.file.StandardCopyOption;
 @Service
 public class AvatarService {
 
+    private static final Logger logger = LoggerFactory.getLogger(AvatarService.class);
+
     private final AvatarRepository avatarRepository;
     private final StudentRepository studentRepository;
     private final Path avatarsDir;
@@ -28,12 +32,20 @@ public class AvatarService {
     }
 
     public Avatar save(Long studentId, MultipartFile file) throws IOException {
+        logger.info("Was invoked method for save avatar for studentId: {}", studentId);
+
         Files.createDirectories(avatarsDir);
         var index = file.getOriginalFilename().lastIndexOf('.');
         var extension = file.getOriginalFilename().substring(index);
-        Path filePath = avatarsDir.resolve(studentId  + extension);
+        Path filePath = avatarsDir.resolve(studentId + extension);
+
+        logger.debug("Saving file to path: {}", filePath);
+
         try (var in = file.getInputStream()) {
             Files.copy(in, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            logger.error("Error saving file for studentId: {}", studentId, e);
+            throw e;
         }
 
         Avatar avatar = avatarRepository.findAllByStudentId(studentId).orElse(new Avatar());
@@ -42,14 +54,18 @@ public class AvatarService {
         avatar.setData(file.getBytes());
         avatar.setStudent(studentRepository.getReferenceById(studentId));
         avatar.setFilePath(filePath.toString());
+
+        logger.info("Saving avatar for studentId: {}", studentId);
         return avatarRepository.save(avatar);
     }
 
     public Avatar getById(Long id) {
+        logger.info("Was invoked method for get avatar by id: {}", id);
         return avatarRepository.findById(id).orElse(null);
     }
 
     public Page<Avatar> findAll(Pageable pageable) {
+        logger.info("Was invoked method for find all avatars with pageable: {}", pageable);
         return avatarRepository.findAll(pageable);
     }
 }
